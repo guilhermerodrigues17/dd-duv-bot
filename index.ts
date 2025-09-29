9fb6a0d6f189dcfa80ed700ee979fb82dff0325d
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-29 20:14:21 -0300

    feat(client): add isReady verification to client instance

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import { baseCollection } from './src/commands/collections/base';
+import fs from 'fs';
+import path from 'path';
 
 const discordToken = Bun.env.DISCORD_TOKEN;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+if (!client) {
+  throw new Error('Client not initialized');
+}
 
 client.once(Events.ClientReady, readyClient => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 client.login(discordToken);
+if (!client.isReady()) {
+  throw new Error('Client not ready');
+}
